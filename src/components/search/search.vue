9b6ba72d7850b332cd5a0fe1b66994b107e5e5fb
supercainiao <1253157617@qq.com>
2019-12-08 17:09:15 +0800

    search

--- src/components/search/search.vue
+++ src/components/search/search.vue
@@ -1,19 +1,36 @@
 <template>
 
-    <div>
+    <div id="search">
         <loginHeader class="loginHeader"></loginHeader>
-        <input type="text">
+        <mt-search
+            v-model="value"
+            cancel-text="取消"
+            placeholder="搜索">
+        </mt-search>
+        <mt-button type="primary">primary</mt-button>
     </div>
 </template>
 
 <script>
 import loginHeader from '../loginHeader'
 export default {
+    data() {
+        return {
+            value:'',
+            result:[
+                'aaa'
+                ,'bbb'
+            ]
+        }
+    },
     components:{
         loginHeader
     }
 }
 </script>
 <style scoped>
-
+    #search>>>.mint-searchbar {
+        border: 1px solid #ddd;
+        border-radius: 10px;
+    }
 </style>
